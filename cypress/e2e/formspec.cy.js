95f29bf08208e2335bcd384cc9029cfd06b718f8
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-05-22 11:23:58 -0700

    Initialized project and integrated Cypress

---
+++ cypress/e2e/formspec.cy.js
@@ -0,0 +1,7 @@
+// Cypress folder created and formspec file created after created a new spec
+
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
